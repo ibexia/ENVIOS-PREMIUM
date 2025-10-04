682e4e8155a35bec2f2fd93a53d8696d1ae835e6
ibexia <xumkox@gmail.com>
2025-10-04 20:10:09 +0200

    Update Premium.py

--- Premium.py
+++ Premium.py
@@ -21,6 +21,8 @@ import pandas_ta as ta
 # ----------------------------------------------------------------------
 tickers = {
     'Acciona': 'ANA.MC',
+    'A3Media': 'A3M.MC'
+    'Adolfo Dominguez': 'ADZ.MC'
     'Accionarenovables': 'ANE.MC',
     'Acerinox': 'ACX.MC',
     'ACS': 'ACS.MC',
@@ -32,12 +34,14 @@ tickers = {
     'Amadeus': 'AMS.MC',
     'Amper': 'AMP.MC',
     'Audax-Renovables': 'ADX.MC',
+    'Atrys Health': 'ATRY.MC'
     'Bankinter': 'BKT.MC',
     'BBVA': 'BBVA.MC',
     'Berkeley': 'BKY.MC',
     'Biotechnology': 'BST.MC',
     'CaixaBank': 'CABK.MC',
     'Cellnex': 'CLNX.MC',
+    'Colonial': 'COL.MC'
     'DIA': 'DIA.MC',
     'Ercros': 'ECR.MC',
     'Endesa': 'ELE.MC',
@@ -51,6 +55,7 @@ tickers = {
     'Fluidra': 'FDR.MC',
     'GAM': 'GAM.MC',
     'Gigas-Hosting': 'GIGA.MC',
+    'Grenergy': 'GRE.MC'
     'Grifols': 'GRF.MC',
     'Grupo San Jose': 'GSJ.MC',
     'Holaluz': 'HLZ.MC',
@@ -77,6 +82,7 @@ tickers = {
     'Pharma-Mar': 'PHM.MC',
     'Prosegur': 'PSG.MC',
     'Puig-brands': 'PUIG.MC',
+    'Realia': 'RLIA.MC'
     'Red-Electrica': 'RED.MC',
     'Repsol': 'REP.MC',
     'Laboratorios-rovi': 'ROVI.MC',
